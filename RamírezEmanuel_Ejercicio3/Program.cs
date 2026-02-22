b6f17c3149a97a080393d008de719c9e7040f56b
Emanuel Ramírez <er6348100@gmail.com>
2026-02-21 18:52:49 -0600

    Menús y datos de entrada

--- RamírezEmanuel_Ejercicio3/Program.cs
+++ RamírezEmanuel_Ejercicio3/Program.cs
@@ -1,1 +1,41 @@
-﻿Console.WriteLine("");
+﻿using System.Threading.Channels;
+
+Console.WriteLine("Seleccione el tipo de cliente: ");
+Console.WriteLine("");
+Console.WriteLine("1. Estudiante");
+Console.WriteLine("2. Docente");
+Console.WriteLine("3. Administrativo");
+Console.WriteLine("4. Externo");
+Console.WriteLine("");
+int cliente=int.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Ingrese el Monto base (Q): ");
+Console.WriteLine("");
+double monto=double.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Ingrese el Método de pago: ");
+Console.WriteLine("");
+Console.WriteLine("1. Efectivo");
+Console.WriteLine("2. Tarjeta");
+Console.WriteLine("3. Transferencia");
+Console.WriteLine("");
+int pago=int.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Tiene cupón? (s/n)");
+Console.WriteLine("");
+string cupon=Console.ReadLine();
+string codigo = "";
+if (cupon=="s")
+{
+    Console.WriteLine("Ingrese el código: ");
+    codigo = Console.ReadLine();
+}
+Console.WriteLine("");
+Console.WriteLine("Ingrese el Reporte antifraude: ");
+Console.WriteLine("");
+Console.WriteLine("1. Ninguno");
+Console.WriteLine("2. Cupón inválido repetido");
+Console.WriteLine("3. Pagos rechazados múltiples");
+Console.WriteLine("");
+int reporte=int.Parse(Console.ReadLine());
+Console.WriteLine("");
